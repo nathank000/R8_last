6d2ac17709ec62eaea3ed2caa7a1c4aed3e5e8cb
Nathan King <nathan@betapoint.co>
2023-06-21 13:23:28 -0400

    cleared

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders a-scene', () => {
-//   const result = render(<App />);
-//   const ascene = result.container.querySelector('#ascene');
-//   expect(ascene).toBeInTheDocument();
-// });
