c68d242960b8004128d0a7bc6fa95c814ad0d16a
Nathan King <nathan@betapoint.co>
2023-07-07 17:31:44 -0400

    putting step 1 back together & app.jsx working

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import StepsContext from './StepsContext';
 import { StepsProvider } from './StepsContext';
 import DataLoader from './components/IO/DataLoader';
 import Step1 from './components/steps/Step1';
+import Step2 from './components/steps/Step2';
+import Step3 from './components/steps/Step3';
+import Step4 from './components/steps/Step4';
+import Step5 from './components/steps/Step5';
+import Step6 from './components/steps/Step6';
+import Step7 from './components/steps/Step7';
 import MainAppView from './components/MainAppView';
 import ProgressHeader from './components/ProgressHeader';
 import logo from './logo.svg';
@@ -28,6 +34,12 @@ const App = () => {
       <LoaderScreen />
       <DataLoader />
       <Step1 />
+      <Step2 />
+      <Step3 />
+      <Step4 />
+      <Step5 />
+      <Step6 />
+      <Step7 />
       <MainAppView />
     </StepsProvider>
   );
--- src/components/steps/Step1.jsx
+++ src/components/steps/Step1.jsx
@@ -19,7 +19,8 @@ function Step1() {
     let stepMainData = APP_DATA.steps[thisStepArrayNum].main_data;
 
     return (
-      <div>
+      <>
+        {step === 1 && (
           <div class="main">
             
             <ProgressHeader />
@@ -27,67 +28,18 @@ function Step1() {
             <div class="d-flex flex-nowrap">
               <LeftColumn />
               <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-light flex-fill">
-                <h1>{ stepTitleText }</h1>
-                
-                {/* https://www.pluralsight.com/guides/return-html-elements-in-json */}
-                
-                <div dangerouslySetInnerHTML={ {__html: stepMainData} } />
-
-                {step === 4 && (
-                  <>  
-                      aframe scene below
-                      <a-scene
-                        tap-place
-                        landing-page="mediaSrc: https://media.giphy.com/media/UIQc7mECaH5nw0Y03Y/giphy.mp4"
-                        xrextras-loading
-                        xrextras-runtime-error
-                        renderer="antialias: true; colorManagement: true; physicallyCorrectLights: true;"
-                        xrweb>
-                        {/* <!-- We can define assets here to be loaded when A-Frame initializes --> */}
-
-                        <a-assets>
-                          {/* <!-- Credit to Poly by Google for the model: https://poly.google.com/view/6pwiq7hSrHr --> */}
-                        
-                          <a-asset-item id="treeModel" src="../../../assets/tree.glb"></a-asset-item>
-                        </a-assets>
-                        
-                        {/* <!-- The raycaster will emit mouse events on scene objects specified with the cantap class --> */}
-                        <a-camera
-                          position="0 8 0"
-                          raycaster="objects: .cantap"
-                          cursor="
-                            fuse: false;
-                            rayOrigin: mouse;">
-                        </a-camera>
-                        <a-entity
-                          light="type: directional;
-                                intensity: 0.8;"
-                          position="1 4.3 2.5">
-                        </a-entity>
-                        <a-light type="ambient" intensity="1"></a-light>
-                        {/* <!-- Adding the cantap class allows the ground to be clicked --> */}
-                        
-                        <a-entity
-                          id="ground"
-                          class="cantap"
-                          geometry="primitive: box"
-                          material="color: #ffffff; transparent: true; opacity: 0.0"
-                          scale="1000 2 1000"
-                          position="0 -1 0">
-                        </a-entity>
-                      </a-scene>
-                  </>
-                )}
-
+                <h1>This is Step { step }</h1>
+                <h1>data = {APP_DATA.campaign.title} </h1>
+                <h1>data = {APP_DATA.campaign.main_image} </h1>
+                (about_data) welcome, this is what this is and you have an opportunity to capture a limited edition token
                 <div>
-                <BottomButtons />
+                  <BottomButtons />
                 </div>
               </div>
-            </div>
           </div>
-      </div>
-    );
-  }
+        }
+      </>
+    ) 
 }
 
 export default Step1;
