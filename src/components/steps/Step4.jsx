2b595ca823b60ccca0697c57fad061ad7572e001
Nathan King <nathan@betapoint.co>
2023-07-09 17:54:23 -0400

    aframe comments removed

--- src/components/steps/Step4.jsx
+++ src/components/steps/Step4.jsx
@@ -24,12 +24,18 @@ function Step4() {
             xrextras-runtime-error
             renderer="antialias: true; colorManagement: true; physicallyCorrectLights: true;"
             xrweb>
-            <!-- We can define assets here to be loaded when A-Frame initializes -->
+            
+            {/* <!-- We can define assets here to be loaded when A-Frame initializes --> */}
+            
             <a-assets>
-              <!-- Credit to Poly by Google for the model: https://poly.google.com/view/6pwiq7hSrHr -->
+              
+              {/* <!-- Credit to Poly by Google for the model: https://poly.google.com/view/6pwiq7hSrHr --> */}
+
               <a-asset-item id="treeModel" src="../../../assets/tree.glb"></a-asset-item>
             </a-assets>
-            <!-- The raycaster will emit mouse events on scene objects specified with the cantap class -->
+
+            {/* <!-- The raycaster will emit mouse events on scene objects specified with the cantap class --> */}
+            
             <a-camera
               position="0 8 0"
               raycaster="objects: .cantap"
@@ -43,7 +49,9 @@ function Step4() {
               position="1 4.3 2.5">
             </a-entity>
             <a-light type="ambient" intensity="1"></a-light>
-            <!-- Adding the cantap class allows the ground to be clicked -->
+
+            {/* <!-- Adding the cantap class allows the ground to be clicked --> */}
+
             <a-entity
               id="ground"
               class="cantap"
