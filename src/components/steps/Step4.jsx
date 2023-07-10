ba9fe26e2e0bb72a1dd20072e62bb05c19040d1e
Nathan King <nathan@betapoint.co>
2023-07-10 16:15:59 -0400

    attempting to work with ARTCOM libraries
    https://github.com/artcom/react-three-arjs

--- src/components/steps/Step4.jsx
+++ src/components/steps/Step4.jsx
@@ -4,40 +4,34 @@ import {Utils} from '../../functions/componentFunctions';
 import ProgressHeader from '../ProgressHeader';
 import LeftColumn from '../LeftColumn';
 import BottomButtons from '../BottomButtons';
+import ReactDOM from "react-dom"
+  
+import { ARCanvas, ARMarker } from "@artcom/react-three-arjs"
+  
 
 
 function Step4() {
   const { step, setStep, name } = useContext(StepsContext);
 
-  return (
-    <div>
-    {step === 4 && (
-      <a-scene
-         renderer="logarithmicDepthBuffer: true;"
-         embedded
-         loading-screen="enabled: false;"
-         arjs="sourceType: webcam; debugUIEnabled: false;"
-     >
-         <a-assets>
-             <a-asset-item
-                 id="animated-asset"
-                 src="../Assets/models/asset.glb"
-             ></a-asset-item>
-         </a-assets>
-
-             <a-entity
-                 look-at="[gps-camera]"
-                 animation-mixer="loop: repeat"
-                 gltf-model="#animated-asset"
-                 scale="1.2998996555779017 1.2998996555779017 1.2998996555779017"
-                 gps-entity-place="latitude: 40.74324157954704; longitude: -73.99424096589156;"
-             ></a-entity>
-
-         <a-camera gps-camera rotation-reader></a-camera>
-      </a-scene>
-    )}
-    </div>
-  );
+  ReactDOM.render(
+    <ARCanvas
+      camera={ { position: [0, 0, 0] } }
+      onCreated={ ({ gl }) => {
+        gl.setSize(window.innerWidth, window.innerHeight)
+      } }>
+      <ambientLight />
+      <pointLight position={ [10, 10, 0] }  />
+      <ARMarker
+        type={ "pattern" }
+        patternUrl={ "data/hiro.patt" }>
+        <mesh>
+          <boxBufferGeometry args={ [1, 1, 1] } />
+          <meshStandardMaterial color={ "green" } />
+        </mesh>
+      </ARMarker>
+    </ARCanvas>,
+    document.getElementById("root")
+  )
 }
 
 export default Step4;
