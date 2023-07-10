09b7aa90f7a337d8d1dd09795a6bac8029bf427b
Nathan King <nathan@betapoint.co>
2023-07-10 17:11:38 -0400

    only if step 4

--- src/components/steps/Step4.jsx
+++ src/components/steps/Step4.jsx
@@ -13,25 +13,27 @@ import { ARCanvas, ARMarker } from "@artcom/react-three-arjs"
 function Step4() {
   const { step, setStep, name } = useContext(StepsContext);
 
-  ReactDOM.render(
-    <ARCanvas
-      camera={ { position: [0, 0, 0] } }
-      onCreated={ ({ gl }) => {
-        gl.setSize(window.innerWidth, window.innerHeight)
-      } }>
-      <ambientLight />
-      <pointLight position={ [10, 10, 0] }  />
-      <ARMarker
-        type={ "pattern" }
-        patternUrl={ "data/hiro.patt" }>
-        <mesh>
-          <boxBufferGeometry args={ [1, 1, 1] } />
-          <meshStandardMaterial color={ "green" } />
-        </mesh>
-      </ARMarker>
-    </ARCanvas>,
-    document.getElementById("root")
-  )
+  {step === 4 && (
+    ReactDOM.render(
+        <ARCanvas
+          camera={ { position: [0, 0, 0] } }
+          onCreated={ ({ gl }) => {
+            gl.setSize(window.innerWidth, window.innerHeight)
+          } }>
+          <ambientLight />
+          <pointLight position={ [10, 10, 0] }  />
+          <ARMarker
+            type={ "pattern" }
+            patternUrl={ "data/hiro.patt" }>
+            <mesh>
+              <boxBufferGeometry args={ [1, 1, 1] } />
+              <meshStandardMaterial color={ "green" } />
+            </mesh>
+          </ARMarker>
+        </ARCanvas>,
+        document.getElementById("root")
+    )
+  )}
 }
 
 export default Step4;
