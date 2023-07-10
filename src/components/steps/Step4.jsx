4708b689aef10559b15a2800857f13ceb5097b35
Nathan King <nathan@betapoint.co>
2023-07-10 08:44:00 -0400

    aframe lat/long example added

--- src/components/steps/Step4.jsx
+++ src/components/steps/Step4.jsx
@@ -12,50 +12,29 @@ function Step4() {
   return (
     <div>
     {step === 4 && (
-          <a-scene
-            tap-place
-            landing-page="mediaSrc: https://media.giphy.com/media/UIQc7mECaH5nw0Y03Y/giphy.mp4"
-            xrextras-loading
-            xrextras-runtime-error
-            renderer="antialias: true; colorManagement: true; physicallyCorrectLights: true;"
-            xrweb>
-            
-            {/* <!-- We can define assets here to be loaded when A-Frame initializes --> */}
-            
-            <a-assets>
-              
-              {/* <!-- Credit to Poly by Google for the model: https://poly.google.com/view/6pwiq7hSrHr --> */}
+      <a-scene
+         renderer="logarithmicDepthBuffer: true;"
+         embedded
+         loading-screen="enabled: false;"
+         arjs="sourceType: webcam; debugUIEnabled: false;"
+     >
+         <a-assets>
+             <a-asset-item
+                 id="animated-asset"
+                 src="../Assets/models/asset.glb"
+             ></a-asset-item>
+         </a-assets>
 
-              <a-asset-item id="treeModel" src="../../../assets/tree.glb"></a-asset-item>
-            </a-assets>
+             <a-entity
+                 look-at="[gps-camera]"
+                 animation-mixer="loop: repeat"
+                 gltf-model="#animated-asset"
+                 scale="1.2998996555779017 1.2998996555779017 1.2998996555779017"
+                 gps-entity-place="latitude: 40.74324157954704; longitude: -73.99424096589156;"
+             ></a-entity>
 
-            {/* <!-- The raycaster will emit mouse events on scene objects specified with the cantap class --> */}
-            
-            <a-camera
-              position="0 8 0"
-              raycaster="objects: .cantap"
-              cursor="
-                fuse: false;
-                rayOrigin: mouse;">
-            </a-camera>
-            <a-entity
-              light="type: directional;
-                    intensity: 0.8;"
-              position="1 4.3 2.5">
-            </a-entity>
-            <a-light type="ambient" intensity="1"></a-light>
-
-            {/* <!-- Adding the cantap class allows the ground to be clicked --> */}
-
-            <a-entity
-              id="ground"
-              class="cantap"
-              geometry="primitive: box"
-              material="color: #ffffff; transparent: true; opacity: 0.0"
-              scale="1000 2 1000"
-              position="0 -1 0">
-            </a-entity>
-          </a-scene>
+         <a-camera gps-camera rotation-reader></a-camera>
+      </a-scene>
     )}
     </div>
   );
