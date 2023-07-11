28189b9476f41f1d9d894676cf14ed7f1d4ecf6d
Nathan King <nathan@betapoint.co>
2023-07-11 16:12:42 -0400

    removing the stuff

--- src/components/steps/Step4.jsx
+++ src/components/steps/Step4.jsx
@@ -6,8 +6,6 @@ import LeftColumn from '../LeftColumn';
 import BottomButtons from '../BottomButtons';
 import ReactDOM from "react-dom"
   
-import { ARCanvas, ARMarker } from "@artcom/react-three-arjs"
-  
 
 
 function Step4() {
@@ -15,22 +13,7 @@ function Step4() {
 
   {step === 4 && (
     ReactDOM.render(
-        <ARCanvas
-          camera={ { position: [0, 0, 0] } }
-          onCreated={ ({ gl }) => {
-            gl.setSize(window.innerWidth, window.innerHeight)
-          } }>
-          <ambientLight />
-          <pointLight position={ [10, 10, 0] }  />
-          <ARMarker
-            type={ "pattern" }
-            patternUrl={ "/Assets/data/hiro.patt" }>
-            <mesh>
-              <boxBufferGeometry args={ [1, 1, 1] } />
-              <meshStandardMaterial color={ "green" } />
-            </mesh>
-          </ARMarker>
-        </ARCanvas>,
+       <h1>F O U R</h1>
         document.getElementById("root")
     )
   )}
