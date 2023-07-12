a52b445b60ce32d867be951092e8eaf09944b610
Nathan King <nathan@betapoint.co>
2023-07-11 22:56:20 -0400

    just so I am not insane

--- src/components/steps/Step4.jsx
+++ src/components/steps/Step4.jsx
@@ -13,6 +13,7 @@ function Step4() {
 
   {step === 4 && (
     <>
+      This is step 4 with the scene
       <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
         <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
         <a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: <your-lat>; longitude: <your-loon>" scale="10 10 10"></a-entity>
